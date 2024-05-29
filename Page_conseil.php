4c594856594879c84f381ccfd3f97ecaa257f611
Myri23 <153503012+Myri23@users.noreply.github.com>
2024-05-29 14:47:43 +0200

    Add files via upload

--- Page_conseil.php
+++ Page_conseil.php
@@ -52,31 +52,31 @@ $conn->close();
 
 
 </head>
-	<header class="navbar">
-		<h1>Kanimy Tuto</h1>
+		<header class="navbar">
+        <img src="title.png" class="logo" width="150px" height="100px">
+    
 
+    <nav>
+        <ul>
+            <li><a href="KanimyTuto_accueil.php">Accueil</a></li>
+            <li><a href="KanimyTuto_accueil.php">A propos de nous</a></li>
+            <li><a href="KanimyTuto_accueil.php">Nous contacter</a></li>
+            <li><a href="Page_conseil.php">Liste de conseils</a></li>
+        </ul>
+    </nav>
 
-	<nav>
-		<ul>
-			<li><a href="KanimyTuto_accueil.php">Accueil</a></li>
-			<li><a href="#about">A propos de nous</a></li>
-			<li><a href="#contact">Nous contacter</a></li>
-		</ul>
-	</nav>
+    <nav class="align-right"> 
+        <a href="KanimyTuto_inscription.php" class="rounded-box">S'inscrire</a>
+        <a href="KanimyTuto_connection.php" class="rounded-box">Se connecter</a>
+    </nav></br>
 
-	<nav class="align-right"> 
-		<a href="KanimyTuto_inscription.php" class="rounded-box">S'inscrire</a>
-		<a href="KanimyTuto_connection.php" class="rounded-box">Se connecter</a>
-	</nav></br>	<br>
-
-	<div class="search-container">
-		<form action="/search" method="GET">
-			<input type="search" name="query" placeholder="Rechercher..." required>
-			<button type="submit" class="rounded-box">Rerchercher</button>
-		</form>
-	</div>
-	</header>
-
+     <div class="search-container">
+        <form method="GET">
+            <input type="search" name="s" placeholder="Rechercher un conseil" value="<?php echo isset($_GET['s']) ? htmlspecialchars($_GET['s']) : ''; ?>">
+            <input type="submit" name="envoyer" value="Rechercher">
+        </form>
+    </div>
+    </header>
 
 <body>
 	
