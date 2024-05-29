6e8ff07c15f76098de292bfc711d3379914f9efb
kanthasi <105271457+kanthasi@users.noreply.github.com>
2024-05-29 12:07:37 +0200

    page conseil

---
+++ Page_conseil.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="3.2" content="width=device-width">
+	<title>page de conseils</title>
+	<link href="KanimyTuto_accueil.css" rel="stylesheet" type="text/css" />
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+</head>
+	<header class="navbar">
+		<h1>Kanimy Tuto</h1>
+
+
+	<nav>
+		<ul>
+			<li><a href="KanimyTuto_accueil.php">Accueil</a></li>
+			<li><a href="#about">A propos de nous</a></li>
+			<li><a href="#contact">Nous contacter</a></li>
+		</ul>
+	</nav>
+
+	<nav class="align-right"> 
+		<a href="#signup" class="rounded-box">S'inscrire</a>
+		<a href="#login" class="rounded-box">Se connecter</a>
+	</nav></br>	<br>
+
+	<div class="search-container">
+		<form action="/search" method="GET">
+			<input type="search" name="query" placeholder="Rechercher..." required>
+			<button type="submit" class="rounded-box">Rerchercher</button>
+		</form>
+	</div>
+	</header>
+
+
+<body>
+	
+
+	<div class="titre">
+ <h1>Page de conseils</h1>
+	</div>
+
+	<div class="catalogue_accueil">
+	<img src="rentree.jpg" >
+ <h4>Bien préparer sa rentrée</h4> 
+		<p>Comment préprer sa rentrée dans les meilleurs conditions on vous montre dans cette article</p>
+<a href="Conseil2.php">
+	<button>Lire la suite</button>
+</a>
+		
+	 </div>
+	 <br><br>
+	<div class="catalogue_accueil">
+		<img src="cheveux.jpg">
+		<h4>Pousse de cheveux</h4>
+		<p>Comment faire pousser les cheveux le plus rapidement grace aux huiles essentielles</p>
+		<a href="Conseil1.php">
+			<button>Lire la suite</button>
+		</a>
+
+
+		 </div>
+
+<br><br>
+		<div class="catalogue_accueil">
+				<img src="Smoothie.jpg" >
+			<h4>Recette de Smoothie aux Fruits Frais en 5 étapes</h4> 
+			<i>Cuisine, Recette, Santé</i> 
+				<p>Besoin d'un coup de fraîcheur pour démarrer votre journée en beauté ou pour vous désaltérer lors des journées chaudes ? Les smoothies aux fruits sont là pour vous ! Dans ce tutoriel, je vous dévoile une recette simple, gorgée de saveurs et de vitamines, pour concocter le smoothie aux fruits frais parfait.</p>
+		<a href="Conseil6.php">
+				<button>Lire la suite</button>
+		</a>
+		</div>
+<br><br>
+		<div class="catalogue_accueil">
+				<img src="Gratitude.png" >
+			<h4>Cultivez la gratitude au quotidien</h4> 
+			<i>Bien-être, Santé</i> 
+				<p>Découvrez comment cultiver un esprit positif et un bien-être général grâce à la pratique de la gratitude. En prenant simplement le temps de reconnaître et d'apprécier les éléments positifs de votre vie, vous ouvrirez la porte à une plus grande satisfaction et à un bonheur durable.</p>   		
+		<a href="Conseil7.php">
+				<button>Lire la suite</button>
+		</a>
+		</div>
+	<br><br>
+		<div class="catalogue_accueil">
+				<img src="Terrarium.jpg" >
+			<h4>Comment créer un terrarium en 6 étapes simples</h4> 
+			<i>Jardinage, Bien-être</i> 
+				<p>Envie d'apporter une touche de nature à votre intérieur tout en créant un petit écosystème enchanteur ? Les terrariums sont la solution parfaite ! Dans ce tutoriel, je vous dévoile comment créer votre propre oasis de verdure en verre. Suivez-moi dans cette aventure verdoyante !</p>   		  
+		<a href="Conseil6.php">
+				<button>Lire la suite</button>
+		</a>
+		</div> 
+		<br><br>
+		<div class="catalogue_accueil">
+				<img src="Illustration_Numérique.jpg" >
+			<h4>Créer une Illustration Numérique de Paysage en 5 Étapes</h4> 
+			<i>Art, Numérique</i> 
+				<p>Dans ce tutoriel, nous allons apprendre à créer une illustration numérique de paysage en utilisant le logiciel de dessin numérique Adobe Photoshop. Ce tutoriel est adapté aux débutants et aux personnes ayant une expérience limitée en illustration numérique.</p>   		  
+		<a href="Conseil3.php">
+				<button>Lire la suite</button>
+		</a>
+		</div> 
+
+<br><br>
+		<div class="catalogue_accueil">
+				<img src="Etagères.jpg" >
+			<h4>Comment Fabriquer une Étagère Murale Flottante en Bois</h4> 
+			<i>Bricolage, Art</i> 
+				<p>Dans ce tutoriel, nous allons vous guider pas à pas dans la création d'une étagère murale flottante en bois. Conçue pour être à la fois élégante et pratique, cette étagère est parfaite pour exposer vos livres, cadres photo et autres décorations. Même si vous êtes débutant en bricolage, vous pouvez facilement réaliser ce projet avec des compétences de base en menuiserie.</p>   		  
+		<a href="Conseil5.php">
+				<button>Lire la suite</button>
+		</a>
+		</div> 
+		<br> <br>
+	</section>
+	<section id="about">
+		<h2>A propos de nous</h2>
+		<p>Notre plateforme, Kanimy Tuto, est bien plus qu'une simple source d'informations. C'est un espace de partage où les utilisateurs peuvent non seulement découvrir une multitude de conseils pratiques dans divers domaines, mais aussi contribuer en partageant leurs propres astuces et connaissances. Que ce soit en proposant une nouvelle recette de smoothie, en partageant des conseils pour cultiver la gratitude ou en dévoilant une technique de bricolage ingénieuse, chaque utilisateur a l'opportunité d'enrichir la communauté par ses connaissances et son expérience. Venez découvrir, apprendre et partager sur Kanimy Tuto, où l'entraide et la créativité sont au cœur de notre philosophie.
+		L'équipe fondatrice de Kanimy Tuto, Kanto A, Inès R et Myriam Saadi, vous souhaite la bienvenue !
+		</p>
+	</section>
+
+	<section id="contact">
+		<h2>Nous contacter</h2>
+		<p>Vous souhaitez nous informer d'un problème sur la plateforme ? Contactez nous par mail au <a href="mailto:">KanimyTuto@gmail.com</a></p>
+	</section>
+</div>
+
+	<footer class="center-align">
+		&copy; 2024 Kanimy Tuto 
+	</footer>
+
+</body>
+
+</html>
